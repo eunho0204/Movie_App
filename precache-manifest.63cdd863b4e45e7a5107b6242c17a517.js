f1134a8c7dfe7a5d6922e232fe733c662a89d0f1
eunho <eunho9195@naver.com>
2020-06-18 10:07:08 +0900

    Updates

---
+++ precache-manifest.63cdd863b4e45e7a5107b6242c17a517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23faff0e818e2a0d13f8b31028f7f815",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "26697ef8b29b7af77376",
+    "url": "/movie_app/static/css/main.a7afb453.chunk.css"
+  },
+  {
+    "revision": "0fda81e216316a75edc8",
+    "url": "/movie_app/static/js/2.8c934742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.8c934742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26697ef8b29b7af77376",
+    "url": "/movie_app/static/js/main.22e6e2a5.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
