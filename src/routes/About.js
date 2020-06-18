0736c7230dcd60574ac6412a5105f4cfb2fb58da
eunho <eunho9195@naver.com>
2020-06-18 14:18:17 +0900

    movie

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About(props) {
+    console.log(props);
+    
+    return(
+        <div className="about__container">
+     <span>i luv movies</span>
+    </div>
+    );
+}
+
+export default About;
